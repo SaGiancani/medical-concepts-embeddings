d633fe4b444009e2622ebe4e682a11c0c1b83fbc
SaGiancani <sa.giancani@gmail.com>
2021-06-11 17:36:44 +0200

    Optimized methods

--- contextual.py
+++ contextual.py
@@ -1,6 +1,7 @@
 import datetime, torch
+import numpy as np
 
-def tokenize_words(tokenizer, sentences):
+def tokenize_words(tokenizer, sentences, start, stop, attention_mask = False):
     #
     #
     #------------------------------------------------------------------------------
@@ -22,25 +23,31 @@ def tokenize_words(tokenizer, sentences):
     a = datetime.datetime.now().replace(microsecond=0)
     
     # Initialize a checkpoint
-    if len(sentences)>10:
-        checkpoint = round(len(sentences)/10)
+    if len(sentences[start:stop])>10:
+        checkpoint = round(len(sentences[start:stop])/10)
         print(checkpoint)
     
-    # initialize dictionary to store tokenized sentences
-    inputs = {'input_ids': [], 'attention_mask': []}
+    # initialize dictionary to store tokenized sentences    
+    inputs = {'input_ids': []}
+ 
+    if attention_mask:
+        inputs['attention_mask'] = []
     
-    for i, sentence in enumerate(sentences):
+    for i, sentence in enumerate(sentences[start:stop]):
         # Encode each sentence and append to dictionary:
         # The truncation is referred to dimension normalization of batches (useless for us)
         # padding = False allows to have tensor of different dimensions depending on the words  
         new_tokens = tokenizer.encode_plus(sentence,truncation=False,
                                            padding=False,return_tensors='pt')
         inputs['input_ids'].append(new_tokens['input_ids'][0])
-        inputs['attention_mask'].append(new_tokens['attention_mask'][0])
+        
+        if attention_mask:
+            inputs['attention_mask'].append(new_tokens['attention_mask'][0])
         
         # Print checkpoint
-        if (i%checkpoint == 0)&(i>0):
-            print(str(round((i/len(sentences))*100))+'% \n')
+        if ((i+1)%checkpoint == 0):
+            print(str(round(((i+1)/len(sentences[start:stop]))*100))+'% \n')
+            
     # reformat list of tensors into single tensor
     # I cant stack the list into a tensor because by construction they have no the same dimension (padding = False)
     #inputs['input_ids'] = torch.stack(inputs['input_ids'])
@@ -68,7 +75,7 @@ def context2static(model, inputs, vocabs, start, stop, name = 'bio_bert', n_laye
     #
     a = datetime.datetime.now().replace(microsecond=0)
     vocabs = vocabs[start:stop]
-    path_save = './Embeddings/words/'+ str(datetime.datetime.now()) + name + '.txt'
+    path_save = './Embeddings/words/'+ str(start)+'--'+str(stop)+name +'-l'+str(n_layer)+ '.txt'
     with open(path_save, 'w') as file:
         # The check is for the first row of static embeddings: they use to take the dimension of written matrix
         if start==0:
@@ -76,26 +83,26 @@ def context2static(model, inputs, vocabs, start, stop, name = 'bio_bert', n_laye
             file.write(''.join('%s %s\n' % (len(inputs), dims)))
 
         # Initialize a checkpoint for printing
-        if len(inputs[start:stop])>100:
-            checkpoint = np.round(len(inputs[start:stop])/100)
+        if len(inputs)>100:
+            checkpoint = np.round(len(inputs)/100)
         else:
             checkpoint = 1
         # Using all the layers for output, the model returns a modeling_outputs object 
         # of 3 elements: we are interested to the last element, a tuple which contains all the layers outputs.
-        for i, inp in enumerate(inputs[start:stop]):
+        for i, inp in enumerate(inputs):
             layer = model(inp.unsqueeze(0))[-1][n_layer]
             #tmp_arr.append(layer.detach().squeeze().numpy().mean(axis=0))
-            oldstr = str(layer.detach().squeeze().numpy().mean(axis=0).tolist())
+            newstr = str(layer.detach().squeeze().numpy().mean(axis=0).tolist())
             #print(np.shape(layer.detach().squeeze().numpy()))
-            newstr = oldstr.replace("]", "")
-            newstr = newstr.replace(",", "")
-            newstr = newstr.replace("[", "")
-            newstr = newstr.replace("'", "")
+            #newstr = newstr.replace("]", "")
+            #newstr = newstr.replace(",", "")
+            #newstr = newstr.replace("[", "")
+            #newstr = newstr.replace("'", "")
             file.write(''.join('%s %s\n' % (vocabs[i], newstr))) 
             # Print checkpoint
             if ((i+1)%checkpoint == 0):
                 print('The layer extraction is at ' + 
-                      str(((i+1)/len(inputs[start:stop]))*100) +
+                      str(((i+1)/len(inputs))*100) +
                       '%: the '+ str(i+1) +'th element'+ '\n')
         print(datetime.datetime.now().replace(microsecond=0)-a)
-        #return tmp_arr
+
