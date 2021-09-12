5526e4a113f0319cb9cf9aac5de08d73f67401e6
SaGiancani <sa.giancani@gmail.com>
2021-09-12 11:41:16 +0200

    Multioperation parsing added - Partial sets_relations and count storing logic discarded

--- analogy_pipeline.py
+++ analogy_pipeline.py
@@ -7,8 +7,16 @@ from gensim.models import KeyedVectors, Word2Vec
 from gensim.test.utils import datapath
 
 
-def analog_loop(path, binary_bool, name, type_emb, L, K, k_most_similar, K_type, logger, analog_comp_dict,
-                sets_relations, dict_labels_for_L = None):
+def analog_loop(path, 
+                binary_bool,
+                name,
+                type_emb,
+                L, K,
+                K_type, logger,
+                analog_comp_dict,
+                #sets_relations,
+                metrics,
+                dict_labels_for_L = None):
     
     # Load the w2v model
     model = KeyedVectors.load_word2vec_format(path, binary=binary_bool)
@@ -34,28 +42,28 @@ def analog_loop(path, binary_bool, name, type_emb, L, K, k_most_similar, K_type,
             
             # sets_relations keeps track of the number of pairs of K and L sets. 
             # The number of filtered pairs on Vemb, per relation are stored
-            sets_relations[rela].append((name+'_l'+K_type, np.shape(l0)))
-            sets_relations[rela].append((name+'_k'+K_type, np.shape(k0)))
+#            sets_relations[rela].append((name+'_l'+K_type, np.shape(l0)))
+#            sets_relations[rela].append((name+'_k'+K_type, np.shape(k0)))
             
             # Save the pickle variable
-            utils.inputs_save(sets_relations, 'Utilities/sets_relations' + name + K_type)
+#            utils.inputs_save(sets_relations, 'Utilities/sets_relations' + name + K_type)
                                 
             # Compute the analogy and store the results
             tmp = measures.analogy_compute(l0, k0, 
                                            model,
-                                           k_most_similar,
+                                           metrics,
                                            logger = logger,
                                            emb_type = 'cui')
             dict_t[name][rela] = tmp
             
             # Compute the sum of analogy hits and store it.
-            if len(tmp)>0:
-                analog_comp_dict[name][rela] = (sum(list(zip(*tmp))[2]), len(tmp))
-            else:
-                analog_comp_dict[name][rela] = (0, len(tmp))                        
+            #if len(tmp)>0:
+            #    analog_comp_dict[name][rela] = (sum(list(zip(*tmp))[2]), len(tmp))
+            #else:
+            #    analog_comp_dict[name][rela] = (0, len(tmp))                        
 
             utils.inputs_save(dict_t, 'Utilities/' + name + K_type)                    
-            utils.inputs_save(analog_comp_dict, 'Utilities/count_analog_' + name + K_type)
+            #utils.inputs_save(analog_comp_dict, 'Utilities/count_analog_' + name + K_type)
             
             # Log of end of 'relation' operation
             logger.info('The time for RELA %s, for embedding %s is %s', 
@@ -79,27 +87,27 @@ def analog_loop(path, binary_bool, name, type_emb, L, K, k_most_similar, K_type,
                                         emb_type = 'labels')
 
             # Store number of filtered pairs
-            sets_relations[rela].append((name+'_l'+K_type, np.shape(l0)))
-            sets_relations[rela].append((name+'_k'+K_type, np.shape(k0)))
+#            sets_relations[rela].append((name+'_l'+K_type, np.shape(l0)))
+#            sets_relations[rela].append((name+'_k'+K_type, np.shape(k0)))
 
-            utils.inputs_save(sets_relations, 'Utilities/sets_relations' + name + K_type)
+#            utils.inputs_save(sets_relations, 'Utilities/sets_relations' + name + K_type)
                     
             tmp = measures.analogy_compute(l0, k0, 
                                            model, 
-                                           k_most_similar, 
+                                           metrics,
                                            logger = logger,
                                            dict_labels_for_L = dict_labels_inters_vemb, 
                                            emb_type = 'labels')                    
                     
             dict_t[name][rela] = tmp
                     
-            if len(tmp)>0:
-                analog_comp_dict[name][rela] = (sum(list(zip(*tmp))[2]), len(tmp))
-            else:
-                analog_comp_dict[name][rela] = (0, len(tmp))
+            #if len(tmp)>0:
+            #    analog_comp_dict[name][rela] = (sum(list(zip(*tmp))[2]), len(tmp))
+            #else:
+            #    analog_comp_dict[name][rela] = (0, len(tmp))
                         
             utils.inputs_save(dict_t, 'Utilities/' + name + K_type)                    
-            utils.inputs_save(analog_comp_dict, 'Utilities/count_analog_' + name + K_type)
+            #utils.inputs_save(analog_comp_dict, 'Utilities/count_analog_' + name + K_type)
                     
             logger.info('The time for RELA %s, for embedding %s is %s', 
                         rela,
@@ -107,15 +115,22 @@ def analog_loop(path, binary_bool, name, type_emb, L, K, k_most_similar, K_type,
                         str(datetime.datetime.now().replace(microsecond=0)-c))
 
     
-def analog_pipe(L, K, k_most_similar, dict_labels_for_L, logger, K_type, parallel = False, embedding_type = 'both'):
+def analog_pipe(L, K,
+                dict_labels_for_L,
+                logger, 
+                K_type,
+                metrics,
+                parallel = False,
+                embedding_type = 'both'):
+    
     a = datetime.datetime.now().replace(microsecond=0)
     
     # Storing expression of relations in sets K and L
-    sets_relations = defaultdict(list)
-    for k in umls_tables_processing.USEFUL_RELA:
-        sets_relations[k].append(('L_umls', np.shape(L[k])))
-        sets_relations[k].append(('K'+K_type, np.shape(K[k])))
-    print('Numbers of pairs for relationships stored')
+#    sets_relations = defaultdict(list)
+#    for k in umls_tables_processing.USEFUL_RELA:
+#        sets_relations[k].append(('L_umls', np.shape(L[k])))
+#        sets_relations[k].append(('K'+K_type, np.shape(K[k])))
+#    print('Numbers of pairs for relationships stored')
     
     # Loading w2v files
     PATH_EMBEDDINGS = './Embeddings'
@@ -146,11 +161,11 @@ def analog_pipe(L, K, k_most_similar, dict_labels_for_L, logger, K_type, paralle
                              os.path.splitext(emb)[0],
                              type_emb[0], 
                              L, K,
-                             k_most_similar,
                              K_type,
                              logger,
                              analog_comp_dict,
-                             sets_relations,
+                             #sets_relations,
+                             metrics,
                              dict_labels_for_L)) 
                 logger.info('Preprocessing finished and multiprocessing running started\n')
                 # Multiprocessing logic for evaluating at the same time K for only copd related concepts
@@ -164,11 +179,11 @@ def analog_pipe(L, K, k_most_similar, dict_labels_for_L, logger, K_type, paralle
                             os.path.splitext(emb)[0],
                             type_emb[0], 
                             L, K,
-                            k_most_similar,
                             K_type,
                             logger,
                             analog_comp_dict,
-                            sets_relations,
+                            #sets_relations,
+                            metrics,
                             dict_labels_for_L)
             
         logger.info('The time for analogical computation of %s is %s', 
@@ -186,7 +201,7 @@ if __name__ == '__main__':
                         type=bool,
                         default = False,
                         required=False,
-                        help='The parallelization switch')
+                        help='The multiprocessing switch')
     
     parser.add_argument('--t',
                         dest='embedding_type',
@@ -209,15 +224,38 @@ if __name__ == '__main__':
                         required=False,
                         help='The choosen L_umls set: False for L, True for L=K')
     
+    parser.add_argument('--K',
+                        dest='k_most_similar',
+                        type=int,
+                        default = 10,
+                        required=False,
+                        help='The choosen k_most_similar value')
+    
+    parser.add_argument('--eps',
+                        dest='eps',
+                        type=float,
+                        default = 0.0001,
+                        required=False,
+                        help='The choosen epsilon value')
+    
+    parser.add_argument('--m', 
+                        nargs='+', 
+                        dest='measure',
+                        default=['add'],
+                        type=str,
+                        help='The requested measures')
+    
     args = parser.parse_args()
     print(args)
     
     # Check on quality of inserted data
+    # Embedding type
     assert args.embedding_type in ['both', 'cuis', 'words'], "Insert a string like 'both', 'cuis', or 'words'"
+    
+    # Measures check
+    assert ('all' in args.measure and len(args.measure)==1) or (len(set(args.measure).intersection(set(['add', 'mul', 'pair']))) == len(args.measure)), "Choose if take 'all' or only certain measures among 'add', 'mul', 'pair'"
 
-    # Constant and logger instantiation    
-    K_MOST_SIMILAR = 10
-
+    # Logger instantiation
     logger = utils.setup_custom_logger('myapp')
     logger.info('Start\n')
     
@@ -226,13 +264,14 @@ if __name__ == '__main__':
         K_umls = umls_tables_processing.count_pairs(umls_tables_processing.USEFUL_RELA, 
                                                          cuis_list = [umls_tables_processing.COPD])
         label_K = '_umls_copd' 
+        
     else:
         # CUIs 
         concepts = umls_tables_processing.concepts_related_to_concept(concept = umls_tables_processing.COPD,
-                                                                  two_way = True,
-                                                                  polishing_rels = False,
-                                                                  switch_key = 'con',
-                                                                  extract_labels = False)
+                                                                      two_way = True,
+                                                                      polishing_rels = False,
+                                                                      switch_key = 'con',
+                                                                      extract_labels = False)
         logger.info('Seeds built\n')
         K_umls = umls_tables_processing.count_pairs(umls_tables_processing.USEFUL_RELA, cuis_list = concepts)
         label_K = '_umls'
@@ -256,13 +295,25 @@ if __name__ == '__main__':
     dict_strings = umls_tables_processing.cui_strings()    
     dict_labels_for_L, _ = umls_tables_processing.extracting_strings(list(tmp), dict_strings = dict_strings)
     logger.info('Dictionary of labels from set L built\n')
+    
+    # Building the dictionary for the measure, in place of the switch-case logic
+    meas_dict = {'add': [measures.cos3add, args.k_most_similar],
+                 'mul': [measures.cos3mul, args.eps], 
+                 'pair': [measures.pair_direction, args.eps]}
+    
+    if 'all' in args.measure:
+        meas_dict = meas_dict
+        
+    else:
+        m = np.sort(args.measure).tolist()
+        meas_dict = {key: meas_dict[key] for key in m if key in meas_dict}
         
     # Start analogy pipeline
     analog_pipe(L_umls, K_umls, 
-                K_MOST_SIMILAR, 
                 dict_labels_for_L, 
                 logger, 
                 label_K, 
+                meas_dict,
                 parallel = args.paralleling,
                 embedding_type = args.embedding_type)
     
